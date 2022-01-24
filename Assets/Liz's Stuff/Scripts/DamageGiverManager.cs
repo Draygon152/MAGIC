848c780d49d8331c23becad50bf248a3c3a702b7
LizTheJelly <97579661+LizTheJelly@users.noreply.github.com>
2022-01-24 00:22:30 -0800

    HealthManager+DamageManager
    Implemented a simple play test map and the follow:
    - Health Manager
    - Damage Manager
    - Player, Enemy, and Object Health Manager
    - Player and object Damage Giver
    - More questions, ask me on discord

---
+++ Assets/Liz's Stuff/Scripts/DamageGiverManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageGiverManager : MonoBehaviour
+{
+    // When a game object (either a player, enemy, or physical object) causes damage to a different
+    // object, it will call its damage function respectively.
+    public int amountOfDamage = 1;
+    public void DamagePlayer(PlayerHealthManager player)
+    {
+        player.LoseHealth(amountOfDamage);
+    }
+
+    public void DamageEnemy(EnemyHealthManager enemy)
+    {
+        enemy.LoseHealth(amountOfDamage);
+    }
+
+    public void DamageObject(ObjectHealthManager obj)
+    {
+        obj.LoseHealth(amountOfDamage);
+    }
+
+    
+}
---
+++ Assets/Liz's Stuff/Scripts/EnemyHealthManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : HealthManager
+{
+    // Currently empty as we do not know if more will added to the Enemy health manager.
+}
