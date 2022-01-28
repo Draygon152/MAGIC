d47f8f77c418648c414966862c8355713750eb9d
AngelRR01 <rubioar@uci.edu>
2022-01-27 19:59:36 -0800

    update

--- Assets/Scripts/Magic/SpellGivesDamage.cs
+++ Assets/Scripts/Magic/SpellGivesDamage.cs
@@ -6,19 +6,21 @@ public class SpellGivesDamage : DamageGiverManager
 {
     [SerializeField] private BaseSpell currentSpell;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Object")
         {
             Debug.Log("Collided Object!");
             ObjectHealthManager obj = collision.gameObject.GetComponentInParent<ObjectHealthManager>();
             DamageObject(obj);
+            Destroy(gameObject); //destroy the spell when it collides
         }
         if (collision.gameObject.tag == "Enemy")
         {
             Debug.Log("Blasted Enemy!");
             EnemyHealthManager enemy = collision.gameObject.GetComponentInParent<EnemyHealthManager>();
             DamageEnemy(enemy, currentSpell.SpellToCast.damage);
+            Destroy(gameObject); //destroy the spell when it collides
         }
     }
 }
