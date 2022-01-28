4144cf428801c6f24046eda0e9074507c41df3b8
Draygon <Raiden152@yahoo.com>
2022-01-28 01:43:21 -0800

    Reorganization of file structure

--- Assets/Scripts/UI/Menus/GameOverMenu.cs
+++ Assets/Scripts/UI/Menus/GameOverMenu.cs
@@ -2,11 +2,6 @@ using UnityEngine;
 
 public class GameOverMenu : Menu<GameOverMenu>
 {
-    private void Awake()
-    {
-        
-    }
-
     public void ReturnToMainMenuPressed()
     {
         //Add code to reset GameManager state back to Start?
