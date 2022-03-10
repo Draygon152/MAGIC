85ea14ecda06c687a1482300c15eecdcfb16ed28
Lizbeth Alcaraz <ldalcara@uci.edu>
2022-03-09 18:11:11 -0800

    Fixed up Nsquel

--- Assets/Scripts/Enemies/EnemyBehavior/NsquelsnaakBehavior.cs
+++ Assets/Scripts/Enemies/EnemyBehavior/NsquelsnaakBehavior.cs
@@ -6,16 +6,10 @@ using UnityEngine;
 
 public class NsquelsnaakBehavior : RangeBehavior
 {
-    //[SerializeField] private float distanceToMaintain = 20.0f;   // The distance the Nsquelsnaak will maintain between itself and the player
-    //[SerializeField] private float maintainDistanceError = 9.0f; // The error from the correct maintain distance that is allowed
-
     [SerializeField] private EnemyBehaviorBase larvaPrefab; // The prefab for spawning the larvae
     [SerializeField] private int spawnTime = 3;             // The time between spawning of larvae
     [SerializeField] private int spawnAmount = 3;           // The amount of larvae that will spawn each time
     [SerializeField] private float spawnRange = 3.0f;       // The maximum range for spawning the larvae
-
-    //private Vector3 targetLocation;   // A variable for storing the target's location 
-    //private float distanceFromTarget; // A float to store the distance between the enemy and the target
 
     private bool spawningLarva = false; // A bool to mark if the Nsquelsnaak is currently spawning larvae
 
