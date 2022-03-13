1810de4eceea8355b9cdd2ff8c094c632dd4a317
Draygon <Raiden152@yahoo.com>
2022-03-12 20:07:42 -0800

    Formatting fix

--- Assets/Scripts/UI/Menus/SoundOptions.cs
+++ Assets/Scripts/UI/Menus/SoundOptions.cs
@@ -1,6 +1,6 @@
 // Written by Marc Hagoriles
+
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Audio;
 
 
@@ -8,13 +8,16 @@ public class SoundOptions : Menu<SoundOptions>
 {
     [SerializeField] private AudioMixer audioMixer;
 
-    //Using the audioMixer, set the game volume relative to the value in the slider.
+
+
+    // Using the audioMixer, set the game volume relative to the value in the slider.
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("gameVolume", volume);
     }
 
-    //When the return button is pressed, close this menu.
+
+    // When the return button is pressed, close this menu.
     public void ReturnPressed()
     {
         Close();
