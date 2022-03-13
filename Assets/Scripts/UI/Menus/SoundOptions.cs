9239ec32730b8ca23e2cd6390b2230cfba64214b
Draygon <Raiden152@yahoo.com>
2022-03-12 20:42:23 -0800

    Tweaking audio implementation

--- Assets/Scripts/UI/Menus/SoundOptions.cs
+++ Assets/Scripts/UI/Menus/SoundOptions.cs
@@ -2,18 +2,32 @@
 
 using UnityEngine;
 using UnityEngine.Audio;
-
+using UnityEngine.UI;
 
 public class SoundOptions : Menu<SoundOptions>
 {
     [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider audioSlider;
+    private static float curVolume = -1.0f;
 
+
+
+    private void Start()
+    {
+        if (curVolume < 0)
+        {
+            curVolume = AudioListener.volume;
+        }
+
+        audioSlider.value = curVolume;
+    }
 
 
     // Using the audioMixer, set the game volume relative to the value in the slider.
     public void SetVolume(float volume)
     {
-        audioMixer.SetFloat("gameVolume", volume);
+        //audioMixer.SetFloat("gameVolume", volume);
+        AudioListener.volume = volume;
     }
 
 
